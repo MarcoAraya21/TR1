4ac6b31e0594bf6bb8d34d89347282b6ce304427
Eduardo <e.osoriovenegas@gmail.com>
2017-06-27 23:54:37 -0400

    Implementando vistas y formularios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up){ |u| u.permit(:email,:Nombre,:Apellido,:Rut,:Direccion,:Telefono, :Fonoalt,:usertype_id, :password,:password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update){ |u| u.permit(:email,:Nombre,:Apellido,:Rut,:Direccion,:Telefono, :Fonoalt,:usertype_id,:password,:password_confirmation)}
+  end
 end
