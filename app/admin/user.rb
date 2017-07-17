1552c3107f7c3806d15070a75f3b2915760866d0
Eduardo <e.osoriovenegas@gmail.com>
2017-07-17 16:34:57 -0400

    Mejoras vista navbar, cproductos, details, recourses, reparations, sales

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation, :usertype_id
+  permit_params :email, :password, :password_confirmation, :usertype_id, :Nombre
 
   index do
     selectable_column
@@ -23,8 +23,7 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "Users Details" do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :Nombre
       f.collection_select  :usertype_id,Usertype.order(:id),:id,:Nombre
     end
     f.actions
